07af1102223c31ca9877079374a12b42b7e0b259
Artem Kulyhin <akulyhin@gmail.com>
2020-07-10 02:10:38 +0300

    Last changes

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,7 @@
 let toggleMenu = document.querySelector('.toggle-menu');
 let menuButton = document.querySelector('.menu__button');
 let closeButton = document.querySelector('.close-btn');
+let menuLinks = document.querySelector('.toggle-menu__list-link');
 
 menuButton.onclick = function() {
     toggleMenu.classList.toggle('active-menu');
@@ -10,4 +11,9 @@ menuButton.onclick = function() {
 closeButton.onclick = function () {
     toggleMenu.classList.toggle('active-menu');
     menuButton.classList.toggle('hidden-menu-button');
+};
+
+menuLinks.onclick = function() {
+    toggleMenu.classList.remove('active-menu');
+    menuButton.classList.remove('hidden-menu-button');
 }
